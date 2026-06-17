a090cbd582f0ad7a34b9858f2017a8068a46ea0a
qinmenghuan <1250397216@qq.com>
2026-06-17 11:23:43 +0800

    feat: udpate database migration cli

--- apps/api/src/common/database/typeorm-cli.config.ts
+++ apps/api/src/common/database/typeorm-cli.config.ts
@@ -6,4 +6,6 @@ const config = databaseConfig();
 
 export default new DataSource({
   ...config,
+  // TypeORM CLI 目前不支持从 NestJS ConfigService 中加载配置，因此需要在这里重复一些配置项，或者直接导入整个配置对象。
+  migrations: ["src/common/database/migrations/*.ts"],
 });
